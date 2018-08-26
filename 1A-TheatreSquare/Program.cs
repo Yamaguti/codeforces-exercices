f11b8d769706cb7268aece252600dc1dafc8e45e
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2018-08-26 20:32:49 -0300

    Improving folder organization and creating VSCode project

---
+++ 1A-TheatreSquare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1A_TheatreSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
