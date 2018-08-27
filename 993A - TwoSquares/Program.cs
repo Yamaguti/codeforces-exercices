b5440def0d5cbc0f270cdcc5a9204d265c92789c
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2018-08-26 21:18:12 -0300

    Added class Square as template

--- 993A - TwoSquares/Program.cs
+++ 993A - TwoSquares/Program.cs
@@ -2,11 +2,34 @@
 
 namespace _993A___TwoSquares
 {
+    class Square {
+        // Constructor
+        public Square() {
+
+        }
+
+        public void print() {
+            Console.WriteLine("aa");
+        }
+
+        public static Square newSquareFromInput()
+        {
+            string[] tokens = Console.ReadLine().Split();
+            return new Square();
+        }
+    };
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Square a = Square.newSquareFromInput();
+            Square b = Square.newSquareFromInput();
+
+            a.print();
+            b.print();
+
+            Console.WriteLine("No");
         }
     }
 }
