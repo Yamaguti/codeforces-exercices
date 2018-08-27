1594077df53991f5dbc701419239efa9a3f7702b
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2018-08-26 21:24:27 -0300

    Organizing high level logic on problem 993A

--- 993A - TwoSquares/Program.cs
+++ 993A - TwoSquares/Program.cs
@@ -4,7 +4,7 @@ namespace _993A___TwoSquares
 {
     class Square {
         // Constructor
-        public Square() {
+        public Square(int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3) {
 
         }
 
@@ -12,10 +12,23 @@ namespace _993A___TwoSquares
             Console.WriteLine("aa");
         }
 
+        public Boolean DoesItIntersects(Square other) {
+            return false;
+        }
+
         public static Square newSquareFromInput()
         {
             string[] tokens = Console.ReadLine().Split();
-            return new Square();
+            return new Square(
+                int.Parse(tokens[0]),
+                int.Parse(tokens[1]),
+                int.Parse(tokens[2]),
+                int.Parse(tokens[3]),
+                int.Parse(tokens[4]),
+                int.Parse(tokens[5]),
+                int.Parse(tokens[6]),
+                int.Parse(tokens[7])
+            );
         }
     };
 
@@ -26,10 +39,11 @@ namespace _993A___TwoSquares
             Square a = Square.newSquareFromInput();
             Square b = Square.newSquareFromInput();
 
-            a.print();
-            b.print();
-
-            Console.WriteLine("No");
+            if (a.DoesItIntersects(b)) {
+                Console.WriteLine("Yes");
+            } else {
+                Console.WriteLine("No");
+            }            
         }
     }
 }
