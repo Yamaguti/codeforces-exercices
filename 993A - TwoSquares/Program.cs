9adecc57418ac13024402afc2ff92af797de58d5
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2018-08-26 21:09:24 -0300

    Added 993A problem folder

---
+++ 993A - TwoSquares/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _993A___TwoSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
