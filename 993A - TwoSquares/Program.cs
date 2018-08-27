cc7734b12a940ed76b16edd8717ca0bf8f8dc2eb
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2018-08-26 22:09:20 -0300

    Formed triangles from square vertex for intersection check

--- 993A - TwoSquares/Program.cs
+++ 993A - TwoSquares/Program.cs
@@ -2,10 +2,39 @@
 
 namespace _993A___TwoSquares
 {
+    class Line {
+        public int x0;
+        public int y0;
+        public int x1;
+        public int y1;
+        
+        public Line(int x0, int y0, int x1, int y1) {
+            this.x0 = x0;
+            this.y0 = y0;
+
+            this.x1 = x1;
+            this.y1 = y1;
+        }
+    }
+
     class Square {
+        public Line l0;
+        public Line l1;
+        public Line l2;
+        public Line l3;
+        public Line da;
+        public Line db;
+
         // Constructor
         public Square(int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3) {
+            l0 = new Line(x0, y0, x1, y1);
+            l1 = new Line(x1, y1, x2, y2);
+            da = new Line(x1, y0, x0, y0);
 
+            
+            l2 = new Line(x2, y2, x3, y3);
+            l3 = new Line(x3, y3, x0, y0);
+            db = new Line(x0, y0, x2, y2);
         }
 
         public void print() {
@@ -13,7 +42,7 @@ namespace _993A___TwoSquares
         }
 
         public Boolean DoesItIntersects(Square other) {
-            return false;
+            
         }
 
         public static Square newSquareFromInput()
